1a9689aaedc3ff6811cc1039d7d199975b0e8071
benzom <31667178+benzom@users.noreply.github.com>
2018-07-10 12:38:42 +0300

    Update file.c

--- file.c
+++ file.c
@@ -4,29 +4,24 @@
 #include <dirent.h> /*library for retrieving information on files and directories*/
 #include <string.h>
 
-int main (int argc, char *argv[])
-{
+int main (int argc, char *argv[]) {
 	DIR *dir; /*represents a directory stream */
 	struct dirent * sd;
 	char mfile[127];
 	
-	if (argc < 2) 
-	{
+	if (argc < 2) {
 		strcpy(mfile, ".");
 	}
 	else
-        strcpy(mfile, argv[1]);
+        	strcpy(mfile, argv[1]);
 
         printf("%s\n\n", mfile);
         dir = opendir(mfile);
 
-	while((sd = readdir(dir)) != NULL)
-	{
+	while((sd = readdir(dir)) != NULL) {
 		printf(">> %s\n", sd->d_name);
 		sd = readdir(dir);
 	}
-
 	closedir(dir);
-
 	return 0;
 }
